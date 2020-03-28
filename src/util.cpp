3dc67e1b85656d2765aa40f4790348e755b1566e
k-vernooy <kai.a.vernooy@gmail.com>
2020-03-28 16:31:45 -0400

    Add color approximation code

---
+++ src/util.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include "../include/util.hpp"
+
+using std::string;
+
+int rgb_to_color(int r, int g, int b) {
+    vector<int> difs;
+
+    for ( vector<int> set : colors ) {
+        difs.push_back(abs(r - set[0]) + abs(g - set[1]) + abs(b - set[2]));
+    }
+
+    int x = distance(difs.begin(), min_element(difs.begin(), difs.end()));
+    return colors[x][3];
+}
